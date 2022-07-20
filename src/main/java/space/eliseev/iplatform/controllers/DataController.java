f09f6a73eca3006524881c03bc4c73f8da23f582
RalfMog <yurispb.mogilevich@gmail.com>
2022-07-20 11:25:27 +0500

    IP-25 1.Реализовал контроллер для ручной загрузки данных с API 2. В Services layer реализовл 2 метода-заглушки

---
+++ src/main/java/space/eliseev/iplatform/controllers/DataController.java
@@ -0,0 +1,28 @@
+package space.eliseev.iplatform.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import space.eliseev.iplatform.services.DataService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(produces = "application/json; charset=UTF-8")
+public class DataController {
+
+    private final DataService dataService;
+
+    @GetMapping("/getFinancial")
+    public ResponseEntity<Void> getFinancial() {
+        dataService.getFinancialData();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/getNonFinancial")
+    public ResponseEntity<Void> getNonFinancial() {
+        dataService.getNonFinancialData();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/space/eliseev/iplatform/services/DataServiceImpl.java
@@ -0,0 +1,17 @@
+package space.eliseev.iplatform.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataServiceImpl implements DataService {
+
+    @Override
+    public String getFinancialData() {
+        return null;
+    }
+
+    @Override
+    public String getNonFinancialData() {
+        return null;
+    }
+}
