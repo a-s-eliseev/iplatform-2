d3f7be1b7a1fc8005ec044d6cd6391f193e5dbea
theSofiChan <101165970+theSofiChan@users.noreply.github.com>
2022-09-14 20:10:03 +0300

    IPM-74 Класс BusinessMessage

--- core/src/main/java/space/eliseev/core/model/BusinessMessage.java
+++ core/src/main/java/space/eliseev/core/model/BusinessMessage.java
@@ -1,19 +1,20 @@
 package space.eliseev.core.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-@Getter
-@Setter
-@RequiredArgsConstructor
-@NoArgsConstructor
+
+@Data
 public class BusinessMessage extends AbstractMessage {
 
-    private final long messageGuid;
+    /**
+     * Identification of the message
+     */
+    private final long messageGuide;
+    /**
+     * The body of a message
+     */
     private final Map<Long, Object> payload = new LinkedHashMap<>();
 }
