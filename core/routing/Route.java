48b896d054f2534755b54c84f1a174a24ac78567
Никита <godunov987@mail.ru>
2022-09-14 14:33:06 +0300

    интерфейс Роутер.

---
+++ core/routing/Route.java
@@ -0,0 +1,12 @@
+package space.eliseev.core.routing;
+/**
+ Интерфейс роутера.
+ */
+public interface Route {
+    /** Запуск роутера
+     */
+    void start();
+    /** Остановка роутера
+      */
+    void stop();
+}
