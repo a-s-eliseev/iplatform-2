eb9eedeff9a0788f64ac7be666ce540303331f8f
theSofiChan <101165970+theSofiChan@users.noreply.github.com>
2022-09-14 19:56:05 +0300

    IPM-73 Абстрактный класс AbstractMessage

--- core/src/main/java/space/eliseev/core/model/AbstractMessage.java
+++ core/src/main/java/space/eliseev/core/model/AbstractMessage.java
@@ -1,21 +1,20 @@
 package space.eliseev.core.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
 
-@Getter
-@Setter
-@RequiredArgsConstructor
-@NoArgsConstructor
+@Data
 public abstract class AbstractMessage {
-
+    /**
+     * Date, time and place where the message was created
+     */
     private ZonedDateTime created;
+    /**
+     * Universally unique identifier for the message
+     */
     private UUID uuid;
 
 }
